928be93e6dc12d9b8532721f976d5965813766ec
TiveCS <creativecode.id@gmail.com>
2022-03-24 20:25:59 +0700

    Add project files.

---
+++ tutor_tp5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutor_tp5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
